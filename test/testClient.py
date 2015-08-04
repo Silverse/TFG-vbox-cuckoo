41732bc0a450e64f64fd1c0a81b62e039ff22cc0
JC <caryaisa5@hotmail.com>
2015-08-04 13:33:28 +0200

    Server and client to test the behaviour of cuckoo

--- test/testClient.py
+++ test/testClient.py
@@ -1,18 +1,51 @@
 
 import socket
+import os
+from ctypes import *
 
+data=['Hi, world', 'wolololo', 'Bye, server!']
+udp_connection=('192.168.56.1', 50001)
+tcp_connection=('192.168.56.1', 50002)
+
+# UDP"
 _sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-f=open('cosa.txt','w+')
+f=open('testClient.txt','w+')
+for string in data:
+	f.write("UDP"+ string)
+	_sock.sendto("UDP"+string, udp_connection)
 
-f.write('Hi, world')
-_sock.sendto('Hi, world!', ('192.168.56.1', 50001))
-f.write('wolololo')
-_sock.sendto('wolololo', ('192.168.56.1', 50001))
-f.write('Bye, server!')
-_sock.sendto('Bye, server!', ('192.168.56.1', 50001))
 
-f.close()
 _sock.close()
 
+# TCP
+_sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+_sock.connect( tcp_connection )
+for string in data:
+	f.write("TCP"+ string)
+	_sock.sent("TCP"+string)
+
+
+_sock.close()
+
+f.close()
+
+# windows API
+f_handle=windll.kernel32.CreateFileA("testtest.txt", 0x10000000,0,0,4,0x80,0)
+written_data = c_int(0)
+windll.kernel32.WriteFile(f_handle, data[0], len(data[0]), byref(written_data),0)
+windll.kernel32.CloseHandle(f_handle)
+
+# Other
+os.system('ping 192.168.56.1')
+os.system('cd')
+os.system('''
+	dir
+	cd "C:\Documents and settings"
+	cd asdfghj
+	cd Escritorio
+	type agent.py
+	''')
+
+	
 exit()
--- test/testServer.py
+++ test/testServer.py
@@ -2,17 +2,44 @@
 
 
 import socket
+import threading
 
-_sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-_sock.bind( ('192.168.56.1',50001) )
-print 'Binded'
+def UDPserver():
+	_sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-for i in range(3):
-	print 'Receiving'
-	(data, addr)=_sock.recvfrom(32)
-	print str(addr)+': '+str(data)
+	_sock.bind( ('192.168.56.1',50001) )
+	print 'Binded'
 
-_sock.close()
+	for i in range(3):
+		print 'Receiving UDP'
+		(data, address)=_sock.recvfrom(32)
+		print str(address)+': '+str(data)
 
-exit()
+	_sock.close()
+	return
+
+def TCPserver():
+	_sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	_sock.bind( ('192.168.56.1',50002) )
+	_sock.listen(1)
+	(clt_sock, address)=_sock.accept()
+	for i in range(3):
+		print 'Receiving TCP'
+		data=clt_sock.recv(32)
+		print str(address)+': '+str(data)
+	_sock.close()
+	clt_sock.close()
+	return
+
+def main():
+	thread_list=[]
+	thread_list.append( threading.Thread(None, UDPserver(), None, None) )
+	thread_list.append( threading.Thread(None, TCPserver(), None, None) )
+	for thread in thread_list:
+		thread.start()
+	
+	return
+
+main()
