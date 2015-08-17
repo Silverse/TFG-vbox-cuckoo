f512920648371c3e811d229d267b03d9c950d2f6
JC <caryaisa5@hotmail.com>
2015-08-17 13:23:31 +0200

    added folder population and minor changes

--- scripts/fakeBrowsing.py
+++ scripts/fakeBrowsing.py
@@ -4,78 +4,128 @@
 # Jose Carlos Ramirez
 # TFG Unizar
 
-# Should be run inside the Guest.  The pre-build IE does not open nothing
+# To be run inside the Guest.  The pre-build IE does not open nothing
+# Some info is for spanish
 
 import os
 import subprocess
 import time
+import random
 
-# Get current date
-proc=subprocess.Popen(["date", '/t'], stdout=subprocess.PIPE, shell=True)
-(_stdout, _stderr)=proc.communicate()
-date=_stdout[:-2].split('/') #day/month/year, in my system, taking out \r\n
-int_date=[int(date[0]),int(date[1]),int(date[2])]
-
-# Get current time
-proc=subprocess.Popen(["time", '/t'], stdout=subprocess.PIPE, shell=True)
-(_stdout, _stderr)=proc.communicate()
-_time=_stdout[:-2].split(':') #hours:minutes taking out \r\n
-int_time=[int(_time[0]),int(_time[1])]
-
-
-web_list=["https://www.facebook.com/", 
-		"https://www.twitter.com", 
-		"https://login.live.com", 
-		"https://www.google.es/search?q=busqueda+de+ejemplo"]
-
-open_IE="start /d "" firefox.exe "#+URL
-close_all_IE="TASKKILL /F /IM firefox.exe /T \r\n"
-
-time_list=[]
-new_time=[1,2]
-for i in range(3):
-	if int_time[0]-i < 0: 
-		new_time[0]=23
-	else:
-		new_time[0]=int_time[0]-i
-	if int_time[1]-i < 0:
-		new_time[1]=59
-	else: 
-		new_time[1]=int_time[1]-i		
-	time_list.append(str(new_time[0])+':'+str(new_time[1]))
-
-date_list=[]
-new_date=[1,2,3]
-for i in range(3):
-	if int_date[0]-i <= 0: #goes one month back
-		new_date[0]=28
-		if int_date[1]-1 <= 0: #goes one year back
-			new_date[1]=12
-			new_date[2]=int_date[2]-1
-		else:
-			new_date[1]=int_date[1]-1
-	else:
-		new_date[0]=int_date[0]-i
-	if int_date[1]-i <= 0:
-		new_time[1]=12		
-	else:
-		new_date[1]=int_date[1]-i	
-	date_list.append(str(new_date[0])+'/'+str(new_date[1])+'/'+str(int_date[2]))
-
-# Every time it changes the time, it opens all the websites, and closes them
-for date in date_list[::-1]:
-        print date
-	os.system("date "+date+"\r\n")
-	for _time in time_list:
-		os.system('time '+_time+'\r\n') 
-		for web in web_list:
-			os.system(open_IE+web+'\r\n')
-			time.sleep(1)
-		os.system(close_all_IE)
+######################### Functions ####################
+# Creates a certain number of files (random names) in each folder
+def populateFolder(folder, n_files, name_size):
+	file_list=[]
+	for n in range(n_file):
+		name=''.join(random.choice(chars) for _ in range(name_size) #random 
+		file_list.append(name)
+			
+	for file_name in file_ist:
+		size=random.randint(100,1000)
+		content=''.join(random.choice(chars) for _ in range(size) #random content
+		_file=open(folder+"\\"+file_name, 'w')
+		_file.write(content)
+		_file.close()
 	
-# Go back to the original one
-os.system('time '+time_list[0]+'\r\n')
-os.system("date "+date_list[0]+"\r\n")
+	return
+# Creates an ordered list of hour:minute
+def timeList(n):
+	# Get current time
+	proc=subprocess.Popen(["time", '/t'], stdout=subprocess.PIPE, shell=True)
+	(_stdout, _stderr)=proc.communicate()
+	_time=_stdout[:-2].split(':') #hours:minutes taking out \r\n
+	int_time=[int(_time[0]),int(_time[1])]
+	# Populate list
+	time_list=[]
+	new_time=[1,2]
+	for i in range(n):
+		if int_time[0]-i < 0: 
+			new_time[0]=23
+		else:
+			new_time[0]=int_time[0]-i
+		if int_time[1]-i < 0:
+			new_time[1]=59
+		else: 
+			new_time[1]=int_time[1]-i		
+		time_list.append(str(new_time[0])+':'+str(new_time[1]))
+	
+	return time_list
+# Creates an ordered list of day/month/year
+def dateList(n):
+	# Get current date
+	proc=subprocess.Popen(["date", '/t'], stdout=subprocess.PIPE, shell=True)
+	(_stdout, _stderr)=proc.communicate()
+	date=_stdout[:-2].split('/') #day/month/year, in my system, taking out \r\n
+	int_date=[int(date[0]),int(date[1]),int(date[2])]
+	# Pupulate list
+	date_list=[]
+	new_date=[1,2,3]
+	for i in range(n):
+		if int_date[0]-i <= 0: #goes one month back
+			new_date[0]=28
+			if int_date[1]-1 <= 0: #goes one year back
+				new_date[1]=12
+				new_date[2]=int_date[2]-1
+			else:
+				new_date[1]=int_date[1]-1
+		else:
+			new_date[0]=int_date[0]-i
+		if int_date[1]-i <= 0:
+			new_date[1]=12		
+		else:
+			new_date[1]=int_date[1]-i	
+		date_list.append(str(new_date[0])+'/'+str(new_date[1])+'/'+str(int_date[2]))
+	
+	return date_list
+# Opens Firefox to visit some webs and then closes it, repear it each time of each date
+def visitWeb(date_list, time_list, web_list):
+	open_IE="start /d "" firefox.exe "#+URL
+	close_all_IE="TASKKILL /F /IM firefox.exe /T \r\n"
+	# Every time it changes the time, it opens all the websites, and closes them
+	for date in date_list[::-1]:
+		os.system("date "+date+"\r\n")
+		for _time in time_list:
+			os.system('time '+_time+'\r\n') 
+			for web in web_list:
+				os.system(open_IE+web+'\r\n')
+				time.sleep(1)
+			os.system(close_all_IE)
+	
+	return
+#######################################################
 
+def main():
+	n_times=3
+	n_files=10
+	name_size=8
+	
+	personal_folder=os.getcwd()
+	while personal_folder[len(personal_folder)-1] != '\\':
+		personal_folder=personal_folder[:-1]
+
+	folder_list=[personal_folder+"Cookies",
+			personal_folder+"Mis\ Documentos",
+			personal_folder+"Mis\ Documentos\\Descargas",
+			'C:\\WINDOWS\Temp']
+	web_list=["https://www.facebook.com/", 
+			"https://www.twitter.com", 
+			"https://login.live.com", 
+			"https://www.google.es/search?q=busqueda+de+ejemplo"]
+	
+	# Fake browsing	
+	date_list=dateList(n_times)
+	time_list=timeList(n_times)
+	visitWeb(date_list, time_list, web_list)
 		
+	# Go back to the original date/time
+	os.system('time '+time_list[0]+'\r\n')
+	os.system("date "+date_list[0]+"\r\n")
+
+	# Populate files
+	for folder in folder_list:
+		populateFolder(folder, n_files, name_size)
+
+	return
+		
+main()		
 exit(0)
--- scripts/main.py
+++ scripts/main.py
@@ -6,34 +6,21 @@
 # TFG Unizar
 
 # General management and run every other script
-# Calls to: 
 
 import os
 import subprocess
 import re
 import random
 
-# Values
-RAM="2000"
-HDD="70000"
-nCores="3"
-file_output="/tmp/newVM.log"
-host_ip="192.168.58.1" 
-default_host_ip="192.168.56.1"
-guest_ip="192.168.58.25" #default 192.168.56.101
-guest_primary_dns="208.67.222.222"
-ftp_port="21"
-
 path_req=os.path.abspath('..')+'/requirements'
 path_cuckoo=os.path.abspath('..')+'/requirements/cuckoo'
 path_bin=os.path.abspath('..')+'/bin'
-path_scripts=s.path.abspath('..')+'/scripts'
-# Current one is /TFG-scripts
+path_scripts=os.path.abspath('..')+'/scripts'
 log_name="main.log"
-
+	
 #################### Functions ###################
 # Check if there is a folder named requirements and if its weight is over a minimum
-def checkIns(folder=path_req): 
+def checkIns(folder): 
 	folder_size = 0
 	for (path, dirs, files) in os.walk(folder):
 		for _file in files:
@@ -42,7 +29,7 @@ def checkIns(folder=path_req):
 	folder_size/=(1024*1024.0)
 	if folder_size < 100: #Less than 100 Mb, +- the weight of a new yara+ssdeep+cuckoo
 		return False
-	if not os.path.isdir(path_req):
+	if not os.path.isdir(folder):
 		return False
 	return True
 # Returns the info about the VMs in the cuckoo conf file
@@ -64,7 +51,7 @@ def getVMlist(system='virtualbox'):
 		line=_file.readline()
 	return	output
 # Returns a non used guest IP
-def newGuestIP(default=guest_ip):
+def newGuestIP(default):
 	ip_found=False
 	vm_list=getVMlist()
 	try:
@@ -145,169 +132,213 @@ def eraseVM(vm_name, system='virtualbox'):
 	tmp_file.close()
 	return	
 
+def checkVif():
+	in_IF=False
+	proc=subprocess.Popen(["vboxmanage", 'list',  'hostonlyifs'], stdout=subprocess.PIPE)
+	(_stdout, _stderr)=proc.communicate()
+	splitted=_stdout.split('\n')
+	for field in splitted:
+			try:
+				re.search('Name:', field).group(0)
+				in_IF=False
+				re.search(' vboxnet0', field).group(0) #without the blank space it does not work because it caches other field
+				in_IF=True
+			except:
+					pass
+			if in_IF:
+				try:
+					re.search('Status:', field).group(0)
+					status=field.split()[1]
+				except:
+					pass
+	
+	return status=="Up"
 #################################################
+def main():
+	# Values
+	RAM="2000"
+	HDD="70000"
+	nCores="3"
+	file_output="/tmp/newVM.log"
+	host_ip="192.168.58.1" 
+	default_host_ip="192.168.56.1"
+	guest_ip="192.168.58.25" #default 192.168.56.101
+	guest_primary_dns="208.67.222.222"
+	ftp_port="21"
 
-# If the output file exists and it was created by a different user, the script won't be able to interact with it (permission denied)
-proc=subprocess.Popen(["ls", '/tmp/'], stdout=subprocess.PIPE)
-(_stdout, _stderr)=proc.communicate()
-try:
-	re.search(file_output[4:], _stdout).group(0)
-	os.system('sudo rm '+file_output) 
-except: #does not exists
-	pass
+	# If the output file exists and it was created by a different user, the script won't be able to interact with it (permission denied)
+	proc=subprocess.Popen(["ls", '/tmp/'], stdout=subprocess.PIPE)
+	(_stdout, _stderr)=proc.communicate()
+	try: # Erase previous tmp file
+		re.search(file_output[4:], _stdout).group(0)
+		os.system('sudo rm '+file_output) 
+	except: #does not exists
+		pass
 
-## Wellcome
-print '''
-	#############################################################
-	####### TFG Ingeniría de Tecnologías y Servicios de   #######
-	####### la Telecomunicación, telemática.	      #######
-	####### Alumno: José Carlos Ramírez Vega, 628545      #######
-	####### Tutor: Antonio Sanz Alcober		      #######
-	####### Ponente: José Luis Salazar Riaño	      #######
-	####### Título: Mejora de la detección de malware     #######
-	####### mediante la modificación profunda de sistemas #######
-	####### de sandboxing.				      #######
-	#############################################################
-'''
+	## Wellcome
+	print '''
+		#############################################################
+		####### TFG Ingeniría de Tecnologías y Servicios de   #######
+		####### la Telecomunicación, telemática.	      #######
+		####### Alumno: José Carlos Ramírez Vega, 628545      #######
+		####### Tutor: Antonio Sanz Alcober		      #######
+		####### Ponente: José Luis Salazar Riaño	      #######
+		####### Título: Mejora de la detección de malware     #######
+		####### mediante la modificación profunda de sistemas #######
+		####### de sandboxing.				      #######
+		#############################################################
+	'''
 
-## Menu
-menu=["\t-1) Install the dependancies and Cuckoo", 
-		"\t-2) Create a new fixed VM",
-		"\t-3) List the Cuckoo's VM", 
-		"\t-4) Run Cuckoo and the webserver (localhost:8080)", 
-		"\t-5) Close" ]
-close=False
-
-while not close:
-	# Print menu
-	if checkIns():
-		marked=False
-	else:	
-		marked=True
-	print "\t###################### Menu ################"
-	for op in menu:
-		if not marked:
-			print op+" [DONE]"
-			marked=True
-		else:
-			print op
-	selection=int(raw_input("Option's number: "))
-
-	# Installation
-	if selection == 1: 
-		# New user
-		if raw_input(' -Do you have a user ready for cuckoo usage? (Y/N): ').upper()=='Y':
-			user_name=raw_input('\t-Please write the user name: ')
-			proc=subprocess.Popen(["whoami"], stdout=subprocess.PIPE)
-			(_stdout, _stderr)=proc.communicate()
-			if _stdout[:-1] != user_name:
-				print "You are not logged as this user. Please change it and run the script again"
-				exit(1)
-		else:
-			user_name=raw_input('\t-Please chose a user name: ')
-			os.system('''
-				sudo adduser  -gecos "" '''+user_name+'''
-				sudo usermod -G vboxusers '''+user_name+'''
-				sudo usermod -g sudo '''+user_name+
-				 '''\n''')
-			print '\n\t-Created: '+user_name+' part of vboxusers and superusers\n-Run the script using the new user.'
-			exit(1)	
-		
-		# Install cuckoo and dependencies
-		if checkIns():
-			if raw_input(" -The requirements folder seems to exist. Do you want to continue? (Y/N): ").upper()=='Y':
-				print "\n [*] Installing Cuckoo, dependancies, and side programs"
-				os.system('python requirements.py '+host_ip)
-		else:
-			print "\n [*] Installing Cuckoo, dependancies, and side programs"
-			os.system('python requirements.py '+host_ip)	
-	# New VM
-	elif selection == 2: 
-		if checkIns(): #Check if (probably) the dependancies are installed
-			vm_name="'"+raw_input("\n -Write the name of your VM: ")+"'"
-			absolute_path=raw_input(" -Please, write down the absolute path of the ISO file of the OS: \n\t")
-			snap_name=raw_input(" -Please chose a snapshot's name: ")
-			
-			# Gets an usable guest's IP
-			host_ip=newGuestIP()
-			
-			# AntiVMdetect execution
-			os.system("sudo python antivmdetect.py "+vm_name+" "+guest_ip+" "+host_ip+" "+guest_primary_dns)
-
-			# FTP, default @IP
-			print "\n [*] Preparing the FTP server (default @IP)"			
-			os.system('sudo service vsftpd stop') # Service's down!
-			os.system("sudo python prepareFTPserver.py "+default_host_ip+" "+ftp_port)			
-			os.system('sudo service vsftpd start > '+log_name) # Service's Up!			
-			#Sometimes it does not end in running state, not sure why so... Check!
-			_file=open(log_name, 'r')
-			line=_file.readline()
-			try:
-				re.search('pre-start', line).group(0)
-				print "WARNING: vsftp did not started properly, restart it manually in other terminal"
-			except:
-				pass
-			_file.close()
-			os.system('rm outTest.txt')
-			
-			os.system('sudo cp '+path_req+'/cuckoo/agent/agent.py /srv/ftp')
-			os.system('sudo cp '+path_bin+'/mouseAct.exe /srv/ftp')
-
-			# VM creation
-			print "\n [*] Creating a VirtualBox's VM named "+vm_name
-			os.system('python newVM-simple.py '+RAM+' '+HDD+' '+nCores+' '+file_output+' '+host_ip+' '+guest_ip+' '+guest_primary_dns+' '+ftp_port+' '+vm_name+' '+absolute_path+' '+snap_name+' '+default_host_ip)
-
-			# FTP, current @IP
-			print "\n [*] Preparing the FTP server (current @IP)"
-			os.system('sudo service vsftpd stop') # Service is down!
-			os.system("sudo python prepareFTPserver.py "+host_ip+" "+ftp_port)
-			os.system('sudo service vsftpd start > '+log_name) # Service is Up!		
-			#Sometimes it does not end in running state, not sure why so... Check!
-			_file=open(log_name, 'r')
-			line=_file.readline()
-			try:
-				re.search('pre-start', line).group(0)
-				print "WARNING: vsftp did not started properly, restart it manually in other terminal"
-			except:
-				pass
-			_file.close()
-			os.system('rm outTest.txt')
-
-			# Cuckoo modifications for the new VM
-			tag_list=raw_input('\n [*] The Cuckoo configuration will be modified to suit the VM\nWrite down a list of tags for cuckoo to add to this VM profile. Separated with white spaces (e.g: windows_xp office_2003 flash_1.2): ')
-			os.system('python cuckooMods.py '+host_ip+' '+guest_ip+' '+vm_name+' '+snap_name+' '+tag_list)
-
-		else:
-			print " The requirements does not seems to be installed, please install them"
-	# List Cuckoo's vms
-	elif selection == 3: 
-		if checkIns():
-			vms=getVMlist()
-			if vms == '':
-				print 'No VMs in the conf file'
+	## Menu
+	_quit=False
+	menu=["\t-1) Install the dependancies and Cuckoo", 
+			"\t-2) Create a new fixed VM",
+			"\t-3) List the Cuckoo's VM", 
+			"\t-4) Run Cuckoo and the webserver (localhost:8080)", 
+			"\t-5) Close" ]	
+	while not _quit:
+		# Print menu
+		print "\t###################### Menu ################"
+		# Show in the menu if the installation is done or not
+		if checkIns(path_req): 
+			marked=False
+		else:	
+			marked=True			
+		for op in menu:
+			if not marked:
+				print op+" [DONE]"
+				marked=True
 			else:
-				print getVMlist()
-		else:
-			print " The requirements does not seems to be installed, please install them"
-	# Run
-	elif selection == 4: 
-		if checkIns():
-			missing_vms=checkVMs()
-			if missing_vms != []:
-				print "WARNING: Some of the VMs listed in Cuckoo's conf file are not in VirtualBox anymore "+str(missing_vms)+" and the file will be sanitized"
-				if raw_input("\tContinue? (Y/N): ").upper()=='Y':
-					for vm in missing_vms:
-						eraseVM(vm[1:-1]) #taking out the quotes
-			#Cuckoo
-			os.system('''gnome-terminal --tab -e "/bin/bash -c 'python '''+path_req+'''/cuckoo/cuckoo.py; exec /bin/bash -i'"''')
-			#Web interface
-			os.system('''gnome-terminal --tab -e "/bin/bash -c 'python '''+path_req+'''/cuckoo/utils/web.py -H localhost -p 8080; exec /bin/bash -i'"''')
-		else:
-			print " The requirements does not seems to be installed, please install them"
-	# Exit
-	else: 
-		close=True
-		print "	Goodbye!"
-	print '\n'
+				print op
+		try:
+			selection=int(raw_input("Option's number: "))
+			# Installation
+			if selection == 1: 
+				# New user
+				if raw_input(' -Do you have a user ready for cuckoo usage? (Y/N): ').upper()=='Y':
+					user_name=raw_input('\t-Please write the user name: ')
+					proc=subprocess.Popen(["whoami"], stdout=subprocess.PIPE)
+					(_stdout, _stderr)=proc.communicate()
+					if _stdout[:-1] != user_name:
+						print "You are not logged as this user. Please change it and run the script again"
+						exit(1)
+				else:
+					user_name=raw_input('\t-Please chose a user name: ')
+					os.system('''
+						sudo adduser  -gecos "" '''+user_name+'''
+						sudo usermod -G vboxusers '''+user_name+'''
+						sudo usermod -g sudo '''+user_name+
+						 '''\n''')
+					print '\n\t-Created: '+user_name+' part of vboxusers and superusers\n-Run the script using the new user.'
+					exit(1)	
+				
+				# Install cuckoo and dependencies
+				if checkIns(path_req):
+					if raw_input(" -The requirements folder seems to exist. Do you want to continue? (Y/N): ").upper()=='Y':
+						print "\n [*] Installing Cuckoo, dependancies, and side programs"
+						os.system('python requirements.py '+host_ip)
+				else:
+					print "\n [*] Installing Cuckoo, dependancies, and side programs"
+					os.system('python requirements.py '+host_ip)	
+			# New VM
+			elif selection == 2: 
+				if checkIns(path_req): #Check if (probably) the dependancies are installed
+					vm_name="'"+raw_input("\n -Write the name of your VM: ")+"'"
+					absolute_path=raw_input(" -Please, write down the absolute path of the ISO file of the OS: \n\t")
+					snap_name=raw_input(" -Please chose a snapshot's name: ")
+					
+					# Gets an usable guest's IP
+					host_ip=newGuestIP(guest_ip)
+					
+					# AntiVMdetect execution
+					os.system("sudo python antivmdetect.py "+vm_name+" "+guest_ip+" "+host_ip+" "+guest_primary_dns)
 
+					# FTP, default @IP
+					print "\n [*] Preparing the FTP server (default @IP)"			
+					os.system('sudo service vsftpd stop') # Service's down!
+					os.system("sudo python prepareFTPserver.py "+default_host_ip+" "+ftp_port)			
+					os.system('sudo service vsftpd start > '+log_name) # Service's Up!			
+					#Sometimes it does not end in running state, not sure why so... Check!
+					_file=open(log_name, 'r')
+					line=_file.readline()
+					try:
+						re.search('pre-start', line).group(0)
+						print "WARNING: vsftp did not started properly, restart it manually in other terminal"
+					except:
+						pass
+					_file.close()
+					os.system('rm outTest.txt')
+					
+					os.system('sudo cp '+path_req+'/cuckoo/agent/agent.py /srv/ftp/agent.pyw') #Hidden window
+					#os.system('sudo cp '+path_bin+'/agent.exe /srv/ftp/agent.exe') #Should be...
+					os.system('sudo cp '+path_bin+'/humanMimic.exe /srv/ftp')
+
+					# VM creation
+					print "\n [*] Creating a VirtualBox's VM named "+vm_name
+					os.system('python newVM-simple.py '+RAM+' '+HDD+' '+nCores+' '+file_output+' '+host_ip+' '+guest_ip+' '+guest_primary_dns+' '+ftp_port+' '+vm_name+' '+absolute_path+' '+snap_name+' '+default_host_ip)
+
+					# FTP, current @IP
+					print "\n [*] Preparing the FTP server (current @IP)"
+					os.system('sudo service vsftpd stop') # Service is down!
+					os.system("sudo python prepareFTPserver.py "+host_ip+" "+ftp_port)
+					os.system('sudo service vsftpd start > '+log_name) # Service is Up!		
+					#Sometimes it does not end in running state, not sure why so... Check!
+					_file=open(log_name, 'r')
+					line=_file.readline()
+					try:
+						re.search('pre-start', line).group(0)
+						print "WARNING: vsftp did not started properly, restart it manually in other terminal"
+					except:
+						pass
+					_file.close()
+					os.system('rm '+log_name)
+
+					# Cuckoo modifications for the new VM
+					tag_list=raw_input('\n [*] The Cuckoo configuration will be modified to suit the VM\nWrite down a list of tags for cuckoo to add to this VM profile. Separated with white spaces (e.g: windows_xp office_2003 flash_1.2): ')
+					os.system('python cuckooMods.py '+host_ip+' '+guest_ip+' '+vm_name+' '+snap_name+' '+tag_list)
+
+				else:
+					print " The requirements does not seems to be installed, please install them"
+			# List Cuckoo's vms
+			elif selection == 3: 
+				if checkIns(path_req):
+					vms=getVMlist()
+					if vms == '':
+						print 'No VMs in the conf file'
+					else:
+						print getVMlist()
+				else:
+					print " The requirements does not seems to be installed, please install them"
+			# Run
+			elif selection == 4: 
+				if checkIns(path_req):
+					missing_vms=checkVMs()
+					if missing_vms != []:
+						print "WARNING: Some of the VMs listed in Cuckoo's conf file are not in VirtualBox anymore "+str(missing_vms)+" and the file will be sanitized"
+						if raw_input("\tContinue? (Y/N): ").upper()=='Y':
+							for vm in missing_vms:
+								eraseVM(vm[1:-1]) #taking out the quotes
+					while not checkVif():
+						print "WARNING: The virtual interface is down. Please start a VM using VboxNet0, this will start the IF (you can turn that VM off whenever you want)"
+						raw_input("Press ENTER when ready. ")
+							
+					#Cuckoo
+					os.system('''gnome-terminal --tab -e "/bin/bash -c 'python '''+path_req+'''/cuckoo/cuckoo.py; exec /bin/bash -i'"''')
+					#Web interface
+					os.system('''gnome-terminal --tab -e "/bin/bash -c 'python '''+path_req+'''/cuckoo/utils/web.py -H localhost -p 8080; exec /bin/bash -i'"''')
+				else:
+					print " The requirements does not seems to be installed, please install them"
+			# Exit
+			elif selection == 5: 
+				_quit=True
+				print "	Goodbye!"
+			# Input selection not in the options
+			else: 
+				print "Wrong input!"
+			print '\n'
+		except ValueError:
+			print "Wrong input!\n"
+	return
+
+main()
 exit(0)
